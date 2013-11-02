0eb1326e764f28fbf0c3d82d501f2ca2e27d4347
Pol Res <arthur.pointeau@gmail.com>
2013-11-02 03:14:48 +0100

    styles coming along

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,21 @@
 module ProjectsHelper
+  def compute_column i
+    if (i == 1)
+      return 4
+    elsif (i == 4 || i==8)
+      return i+4
+    else
+      return 1
+    end
+  end
+
+  def project_row i, p
+
+    render :partial => "projects/project_block", :locals => {:i => i, :p => p}
+  end
+
+  def truncate_description t
+    full_text = link_to(topic.title, topic) + ' - ' + topic.description_without_embed
+
+  end
 end
