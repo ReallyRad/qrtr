96a95f26830513f8265c55afc51effe9f436fd27
ReallyRad <arthur.pointeau@gmail.com>
2013-11-05 22:16:50 +0100

    forgot polymorphic attribute of imageable

--- db/migrate/20131105171555_create_pictures.rb
+++ db/migrate/20131105171555_create_pictures.rb
@@ -2,7 +2,7 @@ class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
       t.string :name
-      t.references :imageable
+      t.references :imageable, :polymorphic => true
       t.string :image
       t.timestamps
     end
